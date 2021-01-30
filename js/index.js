989c7c1997f80b1c08faaf8ffcf1acd6c7a0ad9d
ViktorSeniv <vvitjok@ukr.net>
2021-01-30 15:12:22 +0200

    comit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+const scrollTopButton = document.querySelector(".scrollTopButton");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 600) {
+        scrollTopButton.style.display = "block";
+    } else {
+        scrollTopButton.style.display = "none";
+    }
+};
+
+scrollTopButton.addEventListener("click", scrollToTop);
+function scrollToTop() {
+    window.scrollTo(0, 0);
+  }
+
+
+// Make clock
+const clock = document.querySelector('.clock');
+setInterval(updateClock, 1000);
+function updateClock() {
+    clock.innerText = new Date().toLocaleTimeString();
+}
+
+// Anime LOGO
+var textWrapper = document.querySelector('.ml1 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml1 .letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.ml1 .line',
+    scaleX: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700,
+    offset: '-=875',
+    delay: (el, i, l) => 80 * (l - i)
+  }).add({
+    targets: '.ml1',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  // Logo Carousel
+
+  anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
